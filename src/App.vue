3f12eb063a90b38f887a350dc54a076eaa06d65f
danielecarpentiero <carpentierodaniele@gmail.com>
2024-02-15 17:16:17 +0100

    feat: milestone 3

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <script>
-import header from "./components/Header.vue";
-import main from "./components/Main.vue";
-import footer from "./components/Footer.vue";
-import Header from "./components/Header.vue";
+
 import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
+import Header from "./components/Header.vue";
 export default {
   name: "App",
   components: {
-    header,
-    main,
-    footer
+    Header,
+    Main,
+    Footer
   }
 }
-console.log(Header);
-console.log(Main);
-console.log(footer);
 </script>
 
 <template>
-<header />
-  <main />
-  <footer />
+<Header />
+  <Main />
+  <Footer />
 </template>
 
 <style scoped lang="scss">
