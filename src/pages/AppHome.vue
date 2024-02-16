ef1abbeacd39aa0cd07204cf74c4f085f1b44186
Daniele Carpentiero <carpentierodaniele@gmail.com>
2024-02-17 00:33:36 +0100

    fix: RouterLink instead of router-link

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -11,9 +11,9 @@ export default {
     <h1 class="text-center py-3">My Projects</h1>
     <ul class="row list-unstyled">
       <li class="col col-md-4 g-2" v-for="(project, index) in projects" :key="index">
-        <router-link :to="{name: 'project', params: { slug: project.slug }}">
+        <RouterLink :to="{name: 'project', params: { slug: project.slug }}">
          {{project.name}}
-        </router-link>
+        </RouterLink>
       </li>
     </ul>
   </div>
